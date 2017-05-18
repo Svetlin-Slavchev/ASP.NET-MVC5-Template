8c7908f13e2152866a813634d01aa107f32e77f3
Svetlin Slavchev <tiriron.technology@outlook.com>
2017-05-18 10:26:04 +0300

    Add project files.

---
+++ MVC5Template.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Template.Web.Startup))]
+namespace MVC5Template.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
